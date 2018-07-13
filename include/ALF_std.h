c0750ec50372477b15d50d8a478359641e4a5228
AngheloAlf <angheloalf95@gmail.com>
2018-07-13 12:55:37 -0400

    reestructuring all of this again.

---
+++ include/ALF_std.h
@@ -0,0 +1,13 @@
+#ifndef ALF_std_h
+#define ALF_std_h
+
+#define ALF_std_version (1.0)
+#define ALF_std_version_str ("1.0.0")
+
+#include "ALF_bool.h"
+#include "ALF_dict.h"
+#include "ALF_IO.h"
+#include "ALF_str.h"
+
+#endif /* ALF_std_h */
+
---
+++ include/ALF_std_extra.h
@@ -0,0 +1,10 @@
+#ifndef ALF_std_extra_h
+#define ALF_std_extra_h
+
+#define ALF_std_extra_version (0.1)
+#define ALF_std_extra_version_str ("1.0.0")
+
+#include "ALF_dso_loader.h"
+
+#endif /* ALF_std_extra_h */
+

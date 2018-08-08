c9d40861a48f87a9c85ec5486cb69a85535e216b
Anghelo <angheloalf95@gmail.com>
2018-08-08 11:26:16 -0400

    Added ALF_bit to ALF_std. Ups Fixed a macro warning on some compilers

--- include/ALF_std.h
+++ include/ALF_std.h
@@ -14,12 +14,13 @@
 #define ALF_std_minor_version 0
 #define ALF_std_patch_version 0
 #define ALF_std_version ALF_MAKE_VERSION(ALF_std_major_version, ALF_std_minor_version)
-#define ALF_std_version_str ALF_STR(ALF_std_major_version)"."ALF_STR(ALF_std_minor_version)"."ALF_STR(ALF_std_patch_version)
+#define ALF_std_version_str ALF_STR(ALF_std_major_version) "." ALF_STR(ALF_std_minor_version) "." ALF_STR(ALF_std_patch_version)
 
+#include "ALF_bit.h"
 #include "ALF_bool.h"
 #include "ALF_IO.h"
-#include "ALF_str.h"
 #include "ALF_jit.h"
 #include "ALF_OS.h"
+#include "ALF_str.h"
 
 #endif /* ALF_std_h */
